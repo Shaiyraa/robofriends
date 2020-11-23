5c1679f97dba198bdb32ee9e8b1a097a4964f6fe
Karina Kuss <karina.kuss00@gmail.com>
2020-11-23 04:28:32 +0100

    improved aspects of PWA

--- src/components/searchbox.component.jsx
+++ src/components/searchbox.component.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Searchbox = ({ searchChange }) => (
   <div className="pa2">
     <input className="pa3 ba b--green bg-lightest-blue" 
+    aria-label="Search robots"
     type="search" 
     placeholder="search robots"
     onChange={searchChange} 
